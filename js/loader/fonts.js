f6fd683798ed4442d46838abafe9a423e78c55c6
waxsd100 <wakokara@wax100.io>
2018-05-20 00:26:14 +0900

    画像 / フォント読み込み処理

--- js/loader/fonts.js
+++ js/loader/fonts.js
@@ -6,6 +6,7 @@ fonts_loader
 	.add("undertale", "fonts/undertale.fnt")
 	.add("numbers", "fonts/numbers.fnt")
 	.add("speechbubble", "fonts/speechbubble.fnt")
+	.add("tusb", "fonts/tusb.fnt")
 	.on("complete", function(loader, resources) {
 		console.log(resources);
 		process_fonts(resources);
@@ -17,6 +18,7 @@ function process_fonts(resources) {
 	resources["undertale_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 	resources["numbers_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 	resources["speechbubble_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    resources["tusb_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
 	markAssetLoaded("fonts");
 

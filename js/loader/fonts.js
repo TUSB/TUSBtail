ce80e64f4d6578fd6a61c1e966c2ee1d5fbdf8be
waxsd100 <wakokara@wax100.io>
2018-05-20 18:01:08 +0900

    セリフの日本語化 日本語フォントの追加

--- js/loader/fonts.js
+++ js/loader/fonts.js
@@ -3,23 +3,25 @@
 var fonts_loader = new PIXI.loaders.Loader();
 
 fonts_loader
-	.add("undertale", "fonts/undertale.fnt")
-	.add("numbers", "fonts/numbers.fnt")
-	.add("speechbubble", "fonts/speechbubble.fnt")
-	.add("tusb", "fonts/tusb.fnt")
-	.on("complete", function(loader, resources) {
-		console.log(resources);
-		process_fonts(resources);
-	})
+    .add("undertale", "fonts/undertale.fnt")
+    .add("numbers", "fonts/numbers.fnt")
+    .add("speechbubble", "fonts/speechbubble.fnt")
+    .add("tusb", "fonts/tusb.fnt")
+    .add("ja_jp", "fonts/ja_jp.fnt")
+    .on("complete", function(loader, resources) {
+        console.log(resources);
+        process_fonts(resources);
+    })
 ;
 
 function process_fonts(resources) {
 
-	resources["undertale_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-	resources["numbers_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
-	resources["speechbubble_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    resources["undertale_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    resources["numbers_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    resources["speechbubble_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
     resources["tusb_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+    resources["ja_jp_image"].texture.baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
-	markAssetLoaded("fonts");
+    markAssetLoaded("fonts");
 
 }
